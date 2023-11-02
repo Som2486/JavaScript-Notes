6de63679da53189b20c222bb5a8966579d995bf9
Som2486 <mindflair2486@gmail.com>
2023-11-02 12:03:14 +0530

    Create notes.js
    String, Objects

---
+++ notes.js
@@ -0,0 +1,68 @@
+const name= new String("Somardh Jaiswal")
+const name2=new String("My Name is SJ")
+
+//String Methods
+console.log(name.toUpperCase());
+console.log(name.length)
+console.log(name.indexOf("d"))
+console.log(name)
+console.log(name.substring(0,5))
+console.log(name.slice(-5,5))
+console.log(name2.trim())
+console.log(name2.split(' '))
+console.log(Array.from("Somardh"))
+
+//Object creation
+const juser={
+  name:"Raghav",
+  age:22,
+  city:"Mmbai",
+  email:"xyz@google.in"
+}
+
+console.log(juser["name"])
+console.log(typeof(juser.name))
+
+
+
+//Merging of objects
+//Method1
+const obj1={  1:"a", 2:"b"}
+const obj2={ 3:"b",4:"c" }
+const obj3={5:"d", 6:"e"}
+const obj5=Object.assign({}, obj1,obj2,obj3)
+console.log(obj5)
+
+//Method2
+const obj6={...obj1, ...obj2, ...obj3}
+console.log(obj6)
+
+
+
+//Array of objects to handle database objects
+
+const tinderUser={
+  
+  name: "Raghav",
+  Email:"xyz@gmai.com",
+  isloggedin: "YES"
+}
+console.log(Object.keys(tinderUser))// here  the return type is array which is very helpful
+console.log(Object.values(tinderUser))//here  the return type is array which is very helpful
+console.log(Object.entries(tinderUser))// A array inside array is returned having both keys and values inside
+
+
+
+// Destructring of objects
+
+
+const course={
+  price:999,
+  courseInstructor:"Madhav"
+}
+
+
+// now to acces the given object multiple times and to make code readable
+const{courseInstructor:inst}=course  // now we can use inst to access the values anywhere in the code
+
+console.log(inst)
